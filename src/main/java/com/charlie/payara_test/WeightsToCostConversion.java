fedf1b99c593c1c66b417f161384bbaf14575312
unknown <charlieswires@gmail.com>
2024-09-22 16:20:32 +0100

    minore logic change to be more versatile

--- src/main/java/com/charlie/payara_test/WeightsToCostConversion.java
+++ src/main/java/com/charlie/payara_test/WeightsToCostConversion.java
@@ -1,18 +1,20 @@
 package com.charlie.payara_test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class WeightsToCostConversion {
 	public static final Integer USED_ONCE = 1;
-	public static final Integer USED_MANY = null;
+	public static final Integer USED_MANY = -1;
 
 	public enum RuleNames {
 		FREE_UNDER_7KG_OVERWEIGHT(0.0, 0.0, 7.0, USED_ONCE),
 		FREE_UNDER_25KG_OVERWEIGHT(0.0, 0.0, 25.0, USED_ONCE),
-		FEE_UNDER_7KG_OVERWEIGHT(10.0, 0.0, 7.0, USED_MANY),
+		FEE_UNDER_7KG(10.0, 0.0, 7.0, USED_MANY),
 		FEE_BETWEEN_7KG_AND_25KG_OVERWEIGHT(25.0, 7.0, 25.0, USED_MANY),
 		OVERWEIGHT(5.0, 1.0, null, USED_MANY);
 
@@ -42,18 +44,24 @@ public class WeightsToCostConversion {
 	}
 
 	public static HashMap<String,Integer> singles = new HashMap<String,Integer>();
+	public static List<String> selected = new ArrayList<>();
 	public WeightsToCostConversion() {
 		for(RuleNames item : RuleNames.values()) {
 			singles.put(item.name(),item.getMany());
 		}
+		selected = new ArrayList<>();
 	}
 
 	public void clear() {
 		for(RuleNames item : RuleNames.values()) {
 			singles.put(item.name(),item.getMany());
 		}
+		selected = new ArrayList<>();
 	}
 	
+	public List<String> getSelected(){
+		return selected;
+	}
 	public Double processArrayOfWeights(Double[] weights) {
 		Double sum = 0.0;
 		for(int i = 0; i < weights.length; i++) {
@@ -63,46 +71,49 @@ public class WeightsToCostConversion {
 
 				switch(item.name()) {
 				case "FREE_UNDER_7KG_OVERWEIGHT":
-					if (singles.get("FREE_UNDER_7KG_OVERWEIGHT") == USED_ONCE)
+					if (singles.get("FREE_UNDER_7KG_OVERWEIGHT") > 0)
 						if (weights[i] > item.getOver() && weights[i] < item.getUnder()) {
-							singles.put("FREE_UNDER_7KG_OVERWEIGHT", 0);
+							singles.put("FREE_UNDER_7KG_OVERWEIGHT", singles.get("FREE_UNDER_7KG_OVERWEIGHT") - 1);
+							selected.add(item.name());
 							contin = true;
 						} else if (weights[i] > item.getUnder()) {
 							sum += RuleNames.valueOf("OVERWEIGHT").getPrice() * (weights[i] - item.getUnder());
-							singles.put("FREE_UNDER_7KG_OVERWEIGHT", 0);
+							singles.put("FREE_UNDER_7KG_OVERWEIGHT", singles.get("FREE_UNDER_7KG_OVERWEIGHT") - 1);
+							selected.add(item.name());
 							contin = true;
 						}
 					break;
 				case "FREE_UNDER_25KG_OVERWEIGHT":
-					if (singles.get("FREE_UNDER_25KG_OVERWEIGHT") == USED_ONCE)
+					if (singles.get("FREE_UNDER_25KG_OVERWEIGHT")  > 0)
 						if (weights[i] > item.getOver() && weights[i] < item.getUnder()) {
-							singles.put("FREE_UNDER_25KG_OVERWEIGHT", 0);
+							singles.put("FREE_UNDER_25KG_OVERWEIGHT", singles.get("FREE_UNDER_25KG_OVERWEIGHT") - 1);
+							selected.add(item.name());
 							contin = true;
 						} else if (weights[i] > item.getUnder()) {
 							sum += RuleNames.valueOf("OVERWEIGHT").getPrice() * (weights[i] - item.getUnder());
-							singles.put("FREE_UNDER_25KG_OVERWEIGHT", 0);
+							singles.put("FREE_UNDER_25KG_OVERWEIGHT", singles.get("FREE_UNDER_25KG_OVERWEIGHT") - 1);
+							selected.add(item.name());
 							contin = true;
 						}
 					break;
-				case "FEE_UNDER_7KG_OVERWEIGHT":
-					if (singles.get("FEE_UNDER_7KG_OVERWEIGHT") == USED_MANY)
+				case "FEE_UNDER_7KG":
+					if (singles.get("FEE_UNDER_7KG") == USED_MANY)
 						if (weights[i] > item.getOver() && weights[i] < item.getUnder()) {
 							sum += item.getPrice() * (weights[i]);
+							selected.add(item.name());
 							contin = true;
-						} else if (weights[i] > item.getUnder()) {
-							sum += item.getPrice() * (item.getUnder());
-							sum += RuleNames.valueOf("OVERWEIGHT").getPrice() * (weights[i] - item.getUnder());
-							contin = true;
-						}
+						} 
 					break;
 				case "FEE_BETWEEN_7KG_AND_25KG_OVERWEIGHT":
 					if (singles.get("FEE_BETWEEN_7KG_AND_25KG_OVERWEIGHT") == USED_MANY)
 						if (weights[i] > item.getOver() && weights[i] < item.getUnder()) {
 							sum += item.getPrice() * (weights[i]);
+							selected.add(item.name());
 							contin = true;
 						} else if (weights[i] > item.getUnder()) {
 							sum += item.getPrice() * (item.getUnder());
 							sum += RuleNames.valueOf("OVERWEIGHT").getPrice() * (weights[i] - item.getUnder());
+							selected.add(item.name());
 							contin = true;
 						}
 					break;
--- src/test/java/com/charlie/payara_test/AppTest.java
+++ src/test/java/com/charlie/payara_test/AppTest.java
@@ -22,83 +22,103 @@ public class AppTest {
 
 	private static WeightPermutations wp;
 	private static WeightsToCostConversion wtcc;
-	
+
 	@BeforeAll
 	public static void start() {
 		wp = new WeightPermutations();
 		wtcc = new WeightsToCostConversion();
 	}
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
-    }
-    @Test
-    public void permutations() {
-            // Given array of 5 weights in kilograms
-            Double[] weightsPerm = {5.0, 3.0, 2.0, 4.0, 1.0};
-            
-            // Sort the array in ascending order
-            Arrays.sort(weightsPerm);
-            
-            // Generate and print all permutations
-            wp.generatePermutations(weightsPerm, 0);
-            List<Double[]> outputPerm = wp.getList();
-            wp.clear();
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter("permutations.txt"))) {
-                // Generate and write all permutations
-            	for (int i = 0; i < outputPerm.size(); i++) {
-                writer.write(Arrays.toString(outputPerm.get(i)));
-            	writer.newLine();
-            	}
-                System.out.println("All permutations have been written to permutations.txt");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            List<String> filePermutations = new ArrayList<>();
-            try {
-                filePermutations = Files.readAllLines(Paths.get("permutations.txt"));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            //check the file matches the output
-            for (int i = 0; i < outputPerm.size(); i++) {
-            	assertTrue(Arrays.toString(outputPerm.get(i)).equals(filePermutations.get(i)));
-            }
-            
-            //check there are no duplicates
-            HashSet<Double[]> testSet = new HashSet<Double[]>();
-            testSet.addAll(outputPerm);
-            assertTrue(testSet.size() == 120);
-            
-    }
-    @Test
-    public void calculateCost() {
-        // Given array of 5 weights in kilograms
-        Double[] weights = {35.0, 10.0, 11.0, 4.0, 1.0};
-        // Generate and print all permutations
-        wp.generatePermutations(weights, 0);
-        List<Double[]> output = wp.getList();
-        wp.clear();
-        
-        List<Double> listOfCosts = new ArrayList<>();
-        for(Double[] item : output) {
-        	Double cost = wtcc.processArrayOfWeights(item);
-        	listOfCosts.add(cost);
-        	wtcc.clear();
-        }
-           
-        List<String> filePermutations = new ArrayList<>();
-        try {
-            filePermutations = Files.readAllLines(Paths.get("costs.txt"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    	for (int i = 0; i < output.size(); i++) {
-    		assertTrue((""+listOfCosts.get(i)+Arrays.toString(output.get(i))).equals(filePermutations.get(i)));
-    	}
+	/**
+	 * Rigorous Test :-)
+	 */
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+	@Test
+	public void permutations() {
+		// Given array of 5 weights in kilograms
+		Double[] weightsPerm = {5.0, 3.0, 2.0, 4.0, 1.0};
 
-    }
+		// Sort the array in ascending order
+		Arrays.sort(weightsPerm);
+
+		// Generate and print all permutations
+		wp.generatePermutations(weightsPerm, 0);
+		List<Double[]> outputPerm = wp.getList();
+		wp.clear();
+		//		try (BufferedWriter writer = new BufferedWriter(new FileWriter("permutations.txt"))) {
+		//			// Generate and write all permutations
+		//			for (int i = 0; i < outputPerm.size(); i++) {
+		//				writer.write(Arrays.toString(outputPerm.get(i)));
+		//				writer.newLine();
+		//			}
+		//			System.out.println("All permutations have been written to permutations.txt");
+		//		} catch (IOException e) {
+		//			e.printStackTrace();
+		//		}
+		List<String> filePermutations = new ArrayList<>();
+		try {
+			filePermutations = Files.readAllLines(Paths.get("permutations.txt"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//check the file matches the output
+		for (int i = 0; i < outputPerm.size(); i++) {
+			assertTrue(Arrays.toString(outputPerm.get(i)).equals(filePermutations.get(i)));
+		}
+
+		//check there are no duplicates
+		HashSet<Double[]> testSet = new HashSet<Double[]>();
+		testSet.addAll(outputPerm);
+		assertTrue(testSet.size() == 120);
+
+	}
+	@Test
+	public void calculateCost() {
+		// Given array of 5 weights in kilograms
+		Double[] weights = {35.0, 10.0, 11.0, 4.0, 1.0};
+		// Generate and print all permutations
+		wp.generatePermutations(weights, 0);
+		List<Double[]> output = wp.getList();
+		wp.clear();
+
+		List<Double> listOfCosts = new ArrayList<>();
+		List<List<String>> listOfSelected = new ArrayList<>();
+		for(Double[] item : output) {
+			Double cost = wtcc.processArrayOfWeights(item);
+			listOfCosts.add(cost);
+			listOfSelected.add(wtcc.getSelected());
+			wtcc.clear();
+		}
+//		try (BufferedWriter writer = new BufferedWriter(new FileWriter("costs.txt"))) {
+//			// Generate and write all permutations
+//			for (int i = 0; i < output.size(); i++) {
+//				writer.write(""+listOfCosts.get(i)+Arrays.toString(output.get(i)));
+//				for (String item : listOfSelected.get(i)){
+//					writer.write(""+item+",");
+//				}
+//				writer.newLine();
+//			}
+//			System.out.println("All permutations have been written to costs.txt");
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+
+		List<String> filePermutations = new ArrayList<>();
+		try {
+			filePermutations = Files.readAllLines(Paths.get("costs.txt"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		for (int i = 0; i < output.size(); i++) {
+			StringBuffer sb = new StringBuffer();
+			for (String item : listOfSelected.get(i)){
+				sb.append(""+item+",");
+			}
+
+			assertTrue((""+listOfCosts.get(i)+Arrays.toString(output.get(i))+sb.toString()).equals(filePermutations.get(i)));
+		}
+
+	}
 }
