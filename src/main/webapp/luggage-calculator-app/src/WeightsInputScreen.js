269728288886b186093925180f7119e2c3c4ce09
unknown <charlieswires@gmail.com>
2024-09-23 19:28:37 +0100

    centres 5x person display

--- src/main/webapp/luggage-calculator-app/src/WeightsInputScreen.js
+++ src/main/webapp/luggage-calculator-app/src/WeightsInputScreen.js
@@ -8,48 +8,51 @@ function WeightsInputScreen({ peopleList, onWeightsChange, onWeightsSubmit }) {
 
   return (
     <div>
-      <h1>Enter Weights for Each Person</h1>
-      {peopleList.map((person, personIndex) => (
-        <div
-          key={personIndex}
-          style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}
-        >
-          <span style={{ marginRight: '10px' }}>Person {personIndex + 1}:</span>
-          {person.weights.map((weight, weightIndex) => (
-            <input
-              key={weightIndex}
-              type="number"
-              value={weight}
-              onChange={(e) =>
-                onWeightsChange(personIndex, weightIndex, e.target.value)
-              }
-              style={{ marginRight: '5px', width: '60px' }}
-            />
-          ))}
-          <button
-            onClick={() => handleSubmit(personIndex)}
-            style={{ marginLeft: '10px' }}
+      <h1 style={{ textAlign: 'center' }}>Enter Weights for Each Person</h1>
+      <div style={{ maxWidth: '1000px', margin: '0 auto' }}>
+        {peopleList.map((person, personIndex) => (
+          <div
+            key={personIndex}
+            style={{
+              display: 'grid',
+              gridTemplateColumns: '120px repeat(5, 80px) 100px 120px 220px',
+              alignItems: 'center',
+              marginBottom: '10px',
+              gap: '10px',
+            }}
           >
-            Submit
-          </button>
-          {/* Display Amount Paid */}
-          <input
-            type="text"
-            value={person.amountPaid}
-            readOnly
-            placeholder="Amount Paid"
-            style={{ marginLeft: '10px', width: '100px' }}
-          />
-          {/* Display Transaction ID */}
-          <input
-            type="text"
-            value={person.transactionId}
-            readOnly
-            placeholder="Transaction ID"
-            style={{ marginLeft: '10px', width: '200px' }}
-          />
-        </div>
-      ))}
+            <span style={{ textAlign: 'right' }}>Person {personIndex + 1}:</span>
+            {person.weights.map((weight, weightIndex) => (
+              <input
+                key={weightIndex}
+                type="number"
+                value={weight}
+                onChange={(e) =>
+                  onWeightsChange(personIndex, weightIndex, e.target.value)
+                }
+                style={{ width: '60px' }}
+              />
+            ))}
+            <button onClick={() => handleSubmit(personIndex)}>Submit</button>
+            {/* Display Amount Paid */}
+            <input
+              type="text"
+              value={person.amountPaid}
+              readOnly
+              placeholder="Amount Paid"
+              style={{ width: '100px' }}
+            />
+            {/* Display Transaction ID */}
+            <input
+              type="text"
+              value={person.transactionId}
+              readOnly
+              placeholder="Transaction ID"
+              style={{ width: '200px' }}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
